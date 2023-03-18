005ee3e386e533306baa3cfc357a8076ce72070b
TheHolyRat <ethant8251@gmail.com>
2023-03-17 19:14:06 -0700

    Initial commit. Base files- all modified to fit mod.

---
+++ src/main/java/net/theholyrat/chroniclesofaetheiopeia/ChroniclesOfAetheiopeia.java
@@ -0,0 +1,15 @@
+package net.theholyrat.chroniclesofaetheiopeia;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ChroniclesOfAetheiopeia implements ModInitializer {
+	public static final String MOD_ID = "chroniclesofaetheiopeia";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/theholyrat/chroniclesofaetheiopeia/ChroniclesOfAetheiopeiaClient.java
@@ -0,0 +1,10 @@
+package net.theholyrat.chroniclesofaetheiopeia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChroniclesOfAetheiopeiaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
